c224933632895e76461a9f7ce2f0ac0520bb51bd
Ron J <rojailal@gmail.com>
2022-09-30 15:44:19 -0400

    fix chrome webcam bug

--- Program.cs
+++ Program.cs
@@ -63,7 +63,7 @@ Thread t = new Thread(new ThreadStart(async () =>
     using ServiceProvider serviceProvider = services.BuildServiceProvider();
 
     using (var scope = serviceProvider.CreateScope())
-    {
+    {try{
         var dbcontext = scope.ServiceProvider.GetService<Aisistant.Data.AIAgentDBContext>();
         var apicon = (Aisistant.Controllers.ApiController)Microsoft.Extensions.DependencyInjection.ActivatorUtilities.CreateInstance(scope.ServiceProvider, typeof(Aisistant.Controllers.ApiController));
         await apicon.Log("In the last section, we examined some early aspects of memory. In this section, what we’re going to do is discuss some factors that influence memory. So let’s do that by beginning with the concept on slide two, and that concept is overlearning. Basically in overlearning, the idea is that you continue to study something after you can recall it perfectly. So you study some particular topic whatever that topic is. When you can recall it perfectly, you continue to study it. This is a classic way to help when one is taking comprehensive finals later in the semester. So when you study for exam one and after you really know it all, you continue to study it. That will make your comprehensive final easier.",
@@ -72,7 +72,9 @@ Thread t = new Thread(new ThreadStart(async () =>
 12, 20);
 
         var msgresult=await apicon.GetInterestingMessage();
-
+    }catch(Exception ex){
+        Console.WriteLine(ex.ToString());
+    }
 
     }
 }));
